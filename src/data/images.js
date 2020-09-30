0f220bff5dccdc5447792f214f5bac6e3b7de56a
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-30 12:14:09 +0300

    images json module added + Homepage component

---
+++ src/data/images.js
@@ -0,0 +1,14 @@
+const images = [
+    {
+        alt: "debug-meme",
+        path: "../../gags/debug-gag.png",
+        title: "I Will Fix You"
+    },
+    {
+        alt: "stackoverflow-gag",
+        path: "../../gags/stackoverflow-gag",
+        title: "Shit is true"
+    }
+]
+
+exports.images = images;
